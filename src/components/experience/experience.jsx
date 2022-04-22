85c1f47c49204690b77f7ae50a086de9083eccbb
luis rodriguez <rodluisr@gmail.com>
2022-04-23 00:17:55 +0900

    added experience page and some styles

--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,10 +1,95 @@
 import React from 'react'
 import './experience.css'
+import {BsPatchCheckFill} from "react-icons/bs";
 
 const Experience = () => {
   return (
     <section id='experience'>
-      experience
+      <h5>My Skills</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="eperience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>Gatsby</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="eperience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>Ruby</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>Graphql</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>PostgreSQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>Rails</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+            <article className='experience__details'>
+              <BsPatchCheckFill/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
