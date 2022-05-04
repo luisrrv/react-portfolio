1109e02a67861df9b252967b0e2a600dc013df5a
luis rodriguez <rodluisr@gmail.com>
2022-05-04 23:33:42 +0900

    added footer section

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_m1nvsub', 'template_k9riru8', form.current, 'Zd9PTDulkyMhEhyVg')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+      alert("Thank you, I will reply as soon as possible.")
+  };
+
   return (
     <section id='contact'>
       <h5>Get in Touch</h5>
@@ -25,7 +41,7 @@ const Contact = () => {
           </article>
         </div>
         {/* END OF CONTACT OPTIONS */}
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
           <input type="text" name='name' placeholder='Your Full Name' required />
           <input type="email" name='email' placeholder='Your Email' required />
           <textarea name='message'rows="7" placeholder='Your Message' required ></textarea>
